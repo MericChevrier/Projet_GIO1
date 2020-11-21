f8d75ac77a267f207a7a5ec92af6d32c88154b29
MDTQB <71834688+MDTQB@users.noreply.github.com>
2020-11-21 11:05:27 +0100

    css

--- Final_project/src/components/views/ViewCesium.vue
+++ Final_project/src/components/views/ViewCesium.vue
@@ -8,7 +8,7 @@
 			  	<h1 class="title is-4">Vue3D</h1>
 				  <input class="input is-primary is-small" type="text" placeholder="Text" id="search" name="barre_recherche">
           <button class="button is-small" type="button">Recherche</button>
-			  	<h2 class="subtitle is-5 has-text-weight-semibold">Gestion des couches</h2>
+			  	<h2 class="subtitle is-6 has-text-left has-text-weight-semibold">Gestion des couches</h2>
           <p><label class="checkbox subtitle is-6"><input type="checkbox" name="MNT" onclick="change_MNT(this.checked)"> MNT</label></p>
 			  	<p><label class="checkbox subtitle is-6"><input type="checkbox" name="swissbuilding" onclick="change_swissbuilding(this.checked)"> SwissBuilding</label></p>
 			  	<p><label class="checkbox subtitle is-6"><input type="checkbox" name="volume_implantation" onclick="volume_implantation(this.checked)"> Volume d'implantation</label></p>
@@ -20,18 +20,18 @@
 		  <div id="information_projet">
 		  	<h1 class="title is-4">Information sur le projet</h1>
 		  	<button class="button is-small" type="button" name="import_obj" id="import_obj" onclick="poly_draw()">Importer .obj</button>
-		  	<h2 class="subtitle is-5 has-text-left has-text-weight-semibold">Général</h2>
-		  	<h3 class="subtitle is-6 has-text-left has-text-weight-light">Mensuration officielle</h3>
-		  	<p><label class="checkbox subtitle is-6">Propriétaire :</label></p>
-		  	<p><label class="checkbox subtitle is-6">Porteur du projet :</label></p>
-		  	<p><label class="checkbox subtitle is-6">Parcelle:</label></p>
-		  	<p><label class="checkbox subtitle is-6">Date de mise à l'enquête :</label></p>
-		  	<p><label class="checkbox subtitle is-6">Zone d'affectation :</label></p>
-		  	<p><label class="checkbox subtitle is-6">Surface de plancher :</label></p>
-		  	<p><label class="checkbox subtitle is-6">Surface au sol :</label></p>
+		  	<h2 class="subtitle is-5 has-text-weight-semibold">Général :</h2>
+		  	<h3 class="subtitle is-6 has-text-left has-text-weight-light"><U>Mensuration officielle :</U></h3>
+		  	<p><label class="is-size-7 has-text-black">Propriétaire :</label></p>
+		  	<p><label class="is-size-7 has-text-black">Porteur du projet :</label></p>
+		  	<p><label class="is-size-7 has-text-black">Parcelle:</label></p>
+		  	<p><label class="is-size-7 has-text-black">Date de mise à l'enquête :</label></p>
+		  	<p><label class="is-size-7 has-text-black">Zone d'affectation :</label></p>
+		  	<p><label class="is-size-7 has-text-black">Surface de plancher :</label></p>
+		  	<p><label class="is-size-7 has-text-black">Surface au sol :</label></p>
 		  	<button class="button is-small" type="button" name="validation" id="validation" onclick="poly_draw()">Validation</button>
-		  	<h3 class="subtitle is-5">Respect des restrictions 3D :</h3>
-		  	<p><label class="checkbox subtitle is-6">Volume d'implantation</label></p>
+		  	<h3 class="subtitle is-6"><U>Respect des restrictions 3D :</U></h3>
+		  	<p><label class="is-size-7 has-text-black">Volume d'implantation :</label></p>
 		  	<button class="button is-small" type="button" name="2D" id="2D" onclick="poly_draw()">Passer en mode 2D</button>
 		  </div>
 	  </div>
