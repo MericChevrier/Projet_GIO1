359619f459975b4718074d22ce2d58d04c8d9534
Adrien Bosson <adrien_bosson@hotmail.com>
2020-11-21 10:39:50 +0100

    import fichiers js

--- Final_project/src/main.js
+++ Final_project/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma/css/bulma.css';
+import 'import_geometrie.js';
+
 
 createApp(App).use(router).mount('#app')
