919b724e37f6c322bb6335fb8ddd442f12af2c18
MDTQB <71834688+MDTQB@users.noreply.github.com>
2020-12-05 10:17:50 +0100

    cesuim

--- Projet_Developpement/src/components/views/cesium_import_json.js
+++ Projet_Developpement/src/components/views/cesium_import_json.js
@@ -1,9 +1,10 @@
+import "cesium/Build/Cesium/Widgets/widgets.css";
+import * as Cesium from 'cesium';
+
 export function CesiumImportJson(url){
-    var viewer = new Cesium.Viewer("cesiumContainer");
+    var viewer = new Cesium.Viewer('cesiumContainer');
         viewer.dataSources.add(
-        Cesium.GeoJsonDataSource.load(
-          url
-        )
+        Cesium.GeoJsonDataSource.load(url)
      );
 }
 
