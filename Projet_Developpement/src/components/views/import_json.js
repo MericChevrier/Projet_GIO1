d1a4d9de98bd772a859605591a180bf029592215
MDTQB <71834688+MDTQB@users.noreply.github.com>
2020-12-05 15:38:45 +0100

    cesium

--- Projet_Developpement/src/components/views/import_json.js
+++ Projet_Developpement/src/components/views/import_json.js
@@ -2,6 +2,7 @@ import 'ol/ol.css';
 import Vector from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
+import * as cesium_import_json from './cesium_import_json.js';
 
 export function AddVectorLayer(layer_url,olmap,visibility){
    if (visibility===undefined){visibility=false}
@@ -14,6 +15,7 @@ export function AddVectorLayer(layer_url,olmap,visibility){
         visible: visibility,
         });
     olmap.addLayer(layer)
+    //cesium_import_json.CesiumImportJson(layer_url,viewer)
     return layer
     }
     
